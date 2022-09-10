2202e4dbf988d80ae029789b76883fb85b42ebd6
JorgeRochaA <jor2019.cr@gmail.com>
2022-09-10 01:45:25 -0600

    feat: add logic to display the route

--- src/app/views/delivery/delivery.component.ts
+++ src/app/views/delivery/delivery.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { GoogleMap } from '@angular/google-maps';
+import { MapDirectionsService } from '@angular/google-maps';
+import { Observable, map } from 'rxjs';
+import { PackageResponse } from 'src/app/interfaces/package';
+import { PackageService } from 'src/app/services/package.service';
 
 @Component({
   selector: 'app-delivery',
@@ -8,21 +12,72 @@ import { GoogleMap } from '@angular/google-maps';
 })
 export class DeliveryComponent implements OnInit {
   @ViewChild(GoogleMap) map!: GoogleMap;
+  private directionsRenderer: any;
+  packages!: Array<PackageResponse>;
+  warehouse = { lat: 9.929048216297826, lng: -84.09722946943353 };
   options: google.maps.MapOptions = {
-    center: { lat: 9.929292, lng: -84.096876 },
+    center: this.warehouse,
     zoom: 18,
   };
-  markerOptions: google.maps.MarkerOptions = { draggable: false };
-  markerPosition: google.maps.LatLngLiteral = {
-    lat: 0,
-    lng: 0,
-  };
-  constructor() {}
+  directionsResults$: Observable<google.maps.DirectionsResult | undefined>;
+  wayPoints: any = [];
+  constructor(
+    private mapDirectionsService: MapDirectionsService,
+    private packageService: PackageService
+  ) {
+    const request: google.maps.DirectionsRequest = {
+      origin: this.warehouse,
+      destination: this.warehouse,
+      waypoints: this.wayPoints,
+      optimizeWaypoints: true,
+      travelMode: google.maps.TravelMode.DRIVING,
+    };
+    this.directionsResults$ = mapDirectionsService
+      .route(request)
+      .pipe(map((response) => response.result));
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getPackagesForDelivery();
+  }
 
-  addMarker(event: google.maps.MapMouseEvent) {
-    const { lat, lng } = event.latLng.toJSON();
-    this.markerPosition = { lat, lng };
+  getPackagesForDelivery() {
+    this.packageService.getPackages(2).subscribe({
+      next: (data: any) => {
+        this.packages = data.packages;
+        this.setWayPoints();
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  setWayPoints() {
+    this.packages.forEach((element) => {
+      this.wayPoints.push({
+        location: {
+          lat: Number(element.delivery_to.split(',')[0]),
+          lng: Number(element.delivery_to.split(',')[1]),
+        },
+        stopover: true,
+      });
+    });
+    this.calculateAndDisplayRoute();
+  }
+  calculateAndDisplayRoute() {
+    if (this.wayPoints.length == 0) {
+      this.warehouse = { lat: 0, lng: 0 };
+    }
+    const request: google.maps.DirectionsRequest = {
+      origin: this.warehouse,
+      destination: this.warehouse,
+      waypoints: this.wayPoints,
+      optimizeWaypoints: true,
+      travelMode: google.maps.TravelMode.DRIVING,
+    };
+    this.directionsResults$ = this.mapDirectionsService
+      .route(request)
+      .pipe(map((response) => response.result));
   }
 }
